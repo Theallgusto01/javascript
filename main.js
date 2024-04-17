93cd8f3c56c1f71951e51d1d25dd622478808e02
theallgusto <augusto.dev01@gmail.com>
2024-04-17 08:57:51 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,40 @@
+function toca_som_pom(){
+    document.querySelector("#som_tecla_pom").play()
+}
+
+function toca_som_clap(){
+    document.querySelector("#som_tecla_clap").play()
+}
+
+function toca_som_tim(){
+    document.querySelector("#som_tecla_tim").play()
+}
+function toca_som_puff(){
+    document.querySelector("#som_tecla_puff").play()
+}
+function toca_som_splash(){
+    document.querySelector("#som_tecla_splash").play()
+}
+function toca_som_toim(){
+    document.querySelector("#som_tecla_toim").play()
+}
+function toca_som_psh(){
+    document.querySelector("#som_tecla_psh").play()
+}
+function toca_som_tic(){
+    document.querySelector("#som_tecla_tic").play()
+}
+function toca_som_tom(){
+    document.querySelector("#som_tecla_tom").play()
+}
+
+
+document.querySelector(".tecla_pom").onclick = toca_som_pom
+document.querySelector(".tecla_clap").onclick = toca_som_clap
+document.querySelector(".tecla_tim").onclick = toca_som_tim
+document.querySelector(".tecla_puff").onclick = toca_som_puff
+document.querySelector(".tecla_splash").onclick = toca_som_splash
+document.querySelector(".tecla_toim").onclick = toca_som_toim
+document.querySelector(".tecla_psh").onclick = toca_som_psh
+document.querySelector(".tecla_tic").onclick = toca_som_tic
+document.querySelector(".tecla_tom").onclick = toca_som_tom
