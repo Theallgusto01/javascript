41fccca13e0eef55c84c0cf51024e85eb3a70e1d
theallgusto <augusto.dev01@gmail.com>
2024-04-18 10:40:04 -0300

    melhoramento do código

--- main.js
+++ main.js
@@ -1,40 +1,22 @@
-function toca_som_pom(){
-    document.querySelector("#som_tecla_pom").play()
-}
+    function toca_som(idElementoAudio){
 
-function toca_som_clap(){
-    document.querySelector("#som_tecla_clap").play()
-}
+        document.querySelector(idElementoAudio).play()
 
-function toca_som_tim(){
-    document.querySelector("#som_tecla_tim").play()
-}
-function toca_som_puff(){
-    document.querySelector("#som_tecla_puff").play()
-}
-function toca_som_splash(){
-    document.querySelector("#som_tecla_splash").play()
-}
-function toca_som_toim(){
-    document.querySelector("#som_tecla_toim").play()
-}
-function toca_som_psh(){
-    document.querySelector("#som_tecla_psh").play()
-}
-function toca_som_tic(){
-    document.querySelector("#som_tecla_tic").play()
-}
-function toca_som_tom(){
-    document.querySelector("#som_tecla_tom").play()
-}
+    }
+
+    const lista_de_teclas = document.querySelectorAll('.tecla');
 
 
-document.querySelector(".tecla_pom").onclick = toca_som_pom
-document.querySelector(".tecla_clap").onclick = toca_som_clap
-document.querySelector(".tecla_tim").onclick = toca_som_tim
-document.querySelector(".tecla_puff").onclick = toca_som_puff
-document.querySelector(".tecla_splash").onclick = toca_som_splash
-document.querySelector(".tecla_toim").onclick = toca_som_toim
-document.querySelector(".tecla_psh").onclick = toca_som_psh
-document.querySelector(".tecla_tic").onclick = toca_som_tic
-document.querySelector(".tecla_tom").onclick = toca_som_tom
+
+    for(var i=0; i < lista_de_teclas.length; i++){
+
+        const tecla = lista_de_teclas[i];
+        const instrumento = tecla.classList[1];
+
+        const toca_tecla = "#som_" + instrumento;
+
+        tecla.onclick = function () {
+            toca_som(toca_tecla);
+        }
+    
+    }
