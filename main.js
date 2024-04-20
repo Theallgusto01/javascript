3f5478ea18b3b62186ddf199c57f8765d32f8324
theallgusto <augusto.dev01@gmail.com>
2024-04-20 09:36:26 -0300

    Utilizando Template String

--- main.js
+++ main.js
@@ -6,17 +6,15 @@
 
     const lista_de_teclas = document.querySelectorAll('.tecla');
 
-
-
     for(var i=0; i < lista_de_teclas.length; i++){
 
         const tecla = lista_de_teclas[i];
         const instrumento = tecla.classList[1];
 
-        const toca_tecla = "#som_" + instrumento;
+        // Template String
+        const toca_tecla = `#som_${instrumento}`;
 
         tecla.onclick = function () {
             toca_som(toca_tecla);
-        }
-    
+        } 
     }
